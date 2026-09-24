93e43040eb5ad4b2303fd72737cc3e88cc8e2ebd
Carlo-miguel <carlomiguelesquesta2@gmail.com>
2026-09-24 13:07:41 +0800

    Update AdminLogin.tsx

--- src/pages/admin/AdminLogin.tsx
+++ src/pages/admin/AdminLogin.tsx
@@ -7,7 +7,7 @@ import { Label } from '@/components/ui/label';
 import { useToast } from '@/hooks/use-toast';
 import { Shield, Loader2, Lock, Mail } from 'lucide-react';
 import { z } from 'zod';
-import loginImage from '@/assets/modellogin1.png';
+import loginImage from '@/assets/model8.png';
 import tmcLogo from '/TMClog0s.png';
 
 const loginSchema = z.object({
